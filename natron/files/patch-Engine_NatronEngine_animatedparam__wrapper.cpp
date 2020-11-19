85a0ad831f103694bc26b562e1d0a6cadfc6cbfd
Martinfx <maficccc@gmail.com>
2020-11-19 22:00:32 +0100

    natron: update natron

---
+++ natron/files/patch-Engine_NatronEngine_animatedparam__wrapper.cpp
@@ -0,0 +1,10 @@
+--- Engine/NatronEngine/animatedparam_wrapper.cpp.orig	2020-10-30 05:13:34 UTC
++++ Engine/NatronEngine/animatedparam_wrapper.cpp
+@@ -11,7 +11,6 @@ GCC_DIAG_UNUSED_LOCAL_TYPEDEFS_OFF
+ #include <pysidesignal.h>
+ #include <pysideproperty.h>
+ #include <pyside.h>
+-#include <typeresolver.h>
+ #include <typeinfo>
+ #include "natronengine_python.h"
+ 
---
+++ natron/files/patch-Engine_NatronEngine_natronengine__python.h
@@ -0,0 +1,41 @@
+--- Engine/NatronEngine/natronengine_python.h.orig	2020-10-30 05:13:34 UTC
++++ Engine/NatronEngine/natronengine_python.h
+@@ -4,22 +4,12 @@
+ #define SBK_NATRONENGINE_PYTHON_H
+ 
+ #include <sbkpython.h>
+-#include <conversions.h>
+ #include <sbkenum.h>
+ #include <basewrapper.h>
+ #include <bindingmanager.h>
+ #include <memory>
+ 
+ #include <pysidesignal.h>
+-// Module Includes
+-CLANG_DIAG_OFF(deprecated)
+-CLANG_DIAG_OFF(uninitialized)
+-CLANG_DIAG_OFF(keyword-macro)
+-#include <pyside_qtcore_python.h> // produces warnings
+-CLANG_DIAG_ON(deprecated)
+-CLANG_DIAG_ON(uninitialized)
+-CLANG_DIAG_ON(keyword-macro)
+-
+ // Binded library includes
+ #include <Enums.h>
+ #include <RectD.h>
+@@ -35,7 +25,6 @@ CLANG_DIAG_ON(keyword-macro)
+ // Conversion Includes - Primitive Types
+ #include <QString>
+ #include <signalmanager.h>
+-#include <typeresolver.h>
+ #include <QtConcurrentFilter>
+ #include <QStringList>
+ #include <qabstractitemmodel.h>
+@@ -48,7 +37,6 @@ CLANG_DIAG_ON(keyword-macro)
+ #include <map>
+ #include <QVector>
+ #include <QPair>
+-#include <pysideconversions.h>
+ #include <QSet>
+ #include <vector>
+ #include <QQueue>
