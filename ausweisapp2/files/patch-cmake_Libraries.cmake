288198c73b538d29ee46576f1371cdb9c7781d6f
M <1999714+Martinfx@users.noreply.github.com>
2021-09-16 20:38:18 +0200

    ausweisapp2: update to 1.22.2 (#66)

--- ausweisapp2/files/patch-cmake_Libraries.cmake
+++ ausweisapp2/files/patch-cmake_Libraries.cmake
@@ -1,15 +1,17 @@
---- cmake/Libraries.cmake.orig	2020-01-15 15:59:29 UTC
+--- cmake/Libraries.cmake.orig	2021-09-16 18:07:00 UTC
 +++ cmake/Libraries.cmake
-@@ -66,7 +66,11 @@ IF(MINGW AND NOT CMAKE_CROSSCOMPILING)
- 	SET(CMAKE_CROSSCOMPILING ON)
- ENDIF()
- IF(QT_VENDOR STREQUAL "Governikus" OR FORCE_LEGACY_OPENSSL)
--	FIND_PACKAGE(OpenSSL 1.0.2 REQUIRED) # see openssl_rsa_psk.patch
-+#	FIND_PACKAGE(OpenSSL 1.0.2 REQUIRED) # see openssl_rsa_psk.patch
-+	message("OpenSSL include dir: ${OPENSSL_INCLUDE_DIR}")
-+	message("OpenSSL libraries: ${OPENSSL_LIBRARIES}")
-+	include_directories(${OPENSSL_INCLUDE_DIR})
-+	list(APPEND LIB_LIST ${OPENSSL_LIBRARIES})
- ELSE()
- 	FIND_PACKAGE(OpenSSL 1.1 REQUIRED)
- ENDIF()
+@@ -122,11 +122,9 @@ elseif(MAC)
+ 	find_library(OSX_FOUNDATION Foundation)
+ 	find_library(OSX_SERVICEMANAGEMENT ServiceManagement)
+ elseif(UNIX)
+-	if(LINUX)
+-		find_library(LIBUDEV NAMES udev ludev libudev)
+-		if(NOT LIBUDEV)
+-			message(STATUS "Hardware detection disabled - Missing libudev")
+-		endif()
++	find_library(LIBUDEV NAMES udev ludev libudev)
++	if(NOT LIBUDEV)
++		message(STATUS "Hardware detection disabled - Missing libudev")
+ 	endif()
+ 
+ 	find_package(PkgConfig REQUIRED)
