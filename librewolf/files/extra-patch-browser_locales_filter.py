a88835c898931ba314946a1f4fc4e7bda3a8e08d
Martinfx <maficccc@gmail.com>
2021-09-26 14:09:21 +0200

    librewolf patch extra-patch-browser_locales_filter.py

---
+++ librewolf/files/extra-patch-browser_locales_filter.py
@@ -0,0 +1,10 @@
+--- browser/locales/filter.py.orig	2021-09-26 12:07:06 UTC
++++ browser/locales/filter.py
+@@ -20,7 +20,6 @@ def test(mod, path, entity=None):
+         "browser",
+         "browser/extensions/formautofill",
+         "browser/extensions/fxmonitor",
+-        "browser/extensions/report-site-issue",
+         "extensions/spellcheck",
+         "other-licenses/branding/firefox",
+         "browser/branding/official",
