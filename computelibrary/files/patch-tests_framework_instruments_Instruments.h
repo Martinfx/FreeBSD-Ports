fb405c377f653ad508b1e3e27a311f4f18ab801c
maxfx <maficccc@gmail.com>
2022-10-16 23:08:26 +0200

    compute lib: new port

---
+++ computelibrary/files/patch-tests_framework_instruments_Instruments.h
@@ -0,0 +1,17 @@
+--- tests/framework/instruments/Instruments.h.orig	2022-10-16 20:22:17 UTC
++++ tests/framework/instruments/Instruments.h
+@@ -24,12 +24,12 @@
+ #ifndef ARM_COMPUTE_TEST_INSTRUMENTS
+ #define ARM_COMPUTE_TEST_INSTRUMENTS
+ 
+-#if !defined(_WIN64) && !defined(BARE_METAL) && !defined(__APPLE__) && !defined(__OpenBSD__)
++#if !defined(_WIN64) && !defined(BARE_METAL) && !defined(__APPLE__) && !defined(__OpenBSD__) && !defined(__FreeBSD__)
+ #include "MaliCounter.h"
+ #include "OpenCLMemoryUsage.h"
+ #include "OpenCLTimer.h"
+ #include "PMUCounter.h"
+-#endif /* !defined(_WIN64) && !defined(BARE_METAL) && !defined(__APPLE__) && !defined(__OpenBSD__) */
++#endif /* !defined(_WIN64) && !defined(BARE_METAL) && !defined(__APPLE__) && !defined(__OpenBSD__) && !defined(__OpenBSD__) */
+ #include "SchedulerTimer.h"
+ #include "WallClockTimer.h"
+ 
