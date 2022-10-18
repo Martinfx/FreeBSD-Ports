79914d925717064499bbf021efbad8368c03a1c6
maxfx <maficccc@gmail.com>
2022-10-18 22:05:56 +0200

    max-scale: new port

--- maxscale/files/patch-maxutils_maxbase_src_stacktrace.cc
+++ maxscale/files/patch-maxutils_maxbase_src_stacktrace.cc
@@ -1,33 +1,44 @@
 --- maxutils/maxbase/src/stacktrace.cc.orig	2022-09-12 06:23:56 UTC
 +++ maxutils/maxbase/src/stacktrace.cc
-@@ -18,8 +18,11 @@
- #include <functional>
+@@ -19,7 +19,13 @@
  #include <cstdarg>
  #include <climits>
-+#include <sys/wait.h>
  
++#ifdef __FreeBSD__
++#include <sys/wait.h>  /* For WIF.. */
++#endif
++
 +#ifdef __linux__
  #include <sys/prctl.h>
 +#endif
  
  #ifdef HAVE_GLIBC
  #include <execinfo.h>
-@@ -236,6 +239,7 @@ void emergency_stacktrace(void (* handler)(const char*
+@@ -234,8 +240,16 @@ void emergency_stacktrace(void (* handler)(const char*
+     // We can't dump stacktraces on non-GLIBC systems
+ }
  
++void emergency_stacktrace()
++{
++/*    void* addrs[128];
++    int count = backtrace(addrs, 128);
++    backtrace_symbols_fd(addrs, count, STDOUT_FILENO); */
++}
++
  #endif
  
 +#ifdef __linux__
  void dump_gdb_stacktrace(void (* handler)(const char*))
  {
      prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY);
-@@ -245,6 +249,12 @@ void dump_gdb_stacktrace(void (* handler)(const char*)
+@@ -245,6 +259,12 @@ void dump_gdb_stacktrace(void (* handler)(const char*)
          getpid());
      prctl(PR_SET_PTRACER, 0);
  }
 +#else
 +void dump_gdb_stacktrace(void (* handler)(const char*))
 +{
-+      // we can not use prctl on FreeBSD
++
 +}
 +#endif
  
