2f94ae094aad4037f871c063d7dea898c7291177
maxfx <maficccc@gmail.com>
2022-11-09 15:03:49 +0100

    librewolf: bump to 106.0.5

--- librewolf/files/patch-browser-app-nsBrowserApp.cpp
+++ librewolf/files/patch-browser-app-nsBrowserApp.cpp
@@ -1,10 +1,8 @@
-diff --git browser/app/nsBrowserApp.cpp browser/app/nsBrowserApp.cpp
-index 87c2e16..a41f1d9 100644
---- browser/app/nsBrowserApp.cpp
+--- browser/app/nsBrowserApp.cpp.orig	2022-11-04 13:52:09 UTC
 +++ browser/app/nsBrowserApp.cpp
-@@ -337,6 +337,9 @@ int main(int argc, char* argv[], char* envp[]) {
-   }
- #endif
+@@ -372,6 +372,9 @@ int main(int argc, char* argv[], char* envp[]) {
+   // Register an external module to report on otherwise uncatchable exceptions.
+   CrashReporter::RegisterRuntimeExceptionModule();
  
 +  setenv("MOZ_GMP_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
 +  setenv("MOZ_PLUGIN_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
