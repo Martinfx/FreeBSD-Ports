4e028ae3c8aefde079e66609d998b7a4192a7bb1
maxfx <maficccc@gmail.com>
2024-04-01 18:55:14 +0200

    maxscale: update

--- maxscale/files/patch-cmake_BuildRdkafka.cmake
+++ maxscale/files/patch-cmake_BuildRdkafka.cmake
@@ -1,9 +1,9 @@
---- cmake/BuildRdkafka.cmake.orig	2022-10-08 18:27:50 UTC
+--- cmake/BuildRdkafka.cmake.orig	2023-03-27 10:51:49 UTC
 +++ cmake/BuildRdkafka.cmake
 @@ -13,8 +13,8 @@ ExternalProject_Add(librdkafka
    SOURCE_DIR ${CMAKE_BINARY_DIR}/librdkafka/
    BINARY_DIR ${CMAKE_BINARY_DIR}/librdkafka/
-   CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/librdkafka/configure --prefix=${CMAKE_BINARY_DIR}/librdkafka/ --disable-zstd --disable-sasl --disable-lz4-ext
+   CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/librdkafka/configure --prefix=${CMAKE_BINARY_DIR}/librdkafka/ --disable-zstd --disable-lz4-ext
 -  BUILD_COMMAND make
 -  INSTALL_COMMAND make install
 +  BUILD_COMMAND gmake
