c85074c91525c566c206fd90f406ae949066e338
maxfx <maficccc@gmail.com>
2024-04-22 18:13:18 +0200

    zeno: next work

--- zeno/files/patch-ui_zenodesign_layertreeitemdelegate.cpp
+++ zeno/files/patch-ui_zenodesign_layertreeitemdelegate.cpp
@@ -1,10 +1,18 @@
---- ui/zenodesign/layertreeitemdelegate.cpp.orig	2024-04-22 15:07:50 UTC
+--- ui/zenodesign/layertreeitemdelegate.cpp.orig	2023-11-22 19:17:26 UTC
 +++ ui/zenodesign/layertreeitemdelegate.cpp
-@@ -1,6 +1,7 @@
+@@ -1,7 +1,7 @@
  #include "framework.h"
  #include "layerwidget.h"
  #include "layertreeitemdelegate.h"
+-
 +#include "zenostyle.h"
  
  LayerTreeitemDelegate::LayerTreeitemDelegate(QWidget* parent)
      : QStyledItemDelegate(parent), m_treeview(nullptr)
+@@ -211,4 +211,4 @@ QSize LayerTreeitemDelegate::sizeHint(const QStyleOpti
+ void LayerTreeitemDelegate::initStyleOption(QStyleOptionViewItem* option, const QModelIndex& index) const
+ {
+     QStyledItemDelegate::initStyleOption(option, index);
+-}
+\ No newline at end of file
++}
