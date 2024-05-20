2b74d4cd90a1248ed9e978918ba5d1fea59a16c9
maxfx <maficccc@gmail.com>
2024-05-20 13:42:20 +0200

    mysql-workbench: new port

---
+++ mysql-workbench/files/patch-frontend_linux_workbench_sqlide__form.h
@@ -0,0 +1,11 @@
+--- frontend/linux/workbench/sqlide_form.h.orig	2024-05-20 10:50:12 UTC
++++ frontend/linux/workbench/sqlide_form.h
+@@ -30,7 +30,7 @@
+ #include "form_view_base.h"
+ #include "notebook_dockingpoint.h"
+ #include "overview_panel.h"
+-#include "active_label.h"
++#include "gtk/src/active_label.h"
+ #include "sqlide_output_view.h"
+ #include <glib.h>
+ 
