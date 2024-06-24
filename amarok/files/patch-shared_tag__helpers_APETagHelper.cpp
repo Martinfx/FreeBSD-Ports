f9934e42c93852b8645c838acf1eb629f1fa1b32
maxfx <maficccc@gmail.com>
2024-06-24 20:33:32 +0200

    amarok: new port

--- amarok/files/patch-shared_tag__helpers_APETagHelper.cpp
+++ amarok/files/patch-shared_tag__helpers_APETagHelper.cpp
@@ -1,4 +1,4 @@
---- shared/tag_helpers/APETagHelper.cpp.orig    2024-06-24 08:42:20 UTC
+--- shared/tag_helpers/APETagHelper.cpp.orig	2021-01-31 20:42:44 UTC
 +++ shared/tag_helpers/APETagHelper.cpp
 @@ -84,7 +84,7 @@ APETagHelper::setTags( const Meta::FieldHash &changes 
          QVariant value = changes.value( key );
@@ -9,4 +9,3 @@
          {
              if( key == Meta::valRating )
                  m_tag->addValue( field, Qt4QStringToTString( QString::number( value.toFloat() / 10.0 ) ) );
- 
