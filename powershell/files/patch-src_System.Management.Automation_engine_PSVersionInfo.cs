631fe824722192fd6c0ced2dede45c83e8864b36
M. <1999714+Martinfx@users.noreply.github.com>
2024-09-28 16:45:43 +0200

    update powershell (#157)
    * powershell: new port

--- powershell/files/patch-src_System.Management.Automation_engine_PSVersionInfo.cs
+++ powershell/files/patch-src_System.Management.Automation_engine_PSVersionInfo.cs
@@ -1,4 +1,4 @@
---- src/System.Management.Automation/engine/PSVersionInfo.cs.orig	2024-09-21 21:10:48 UTC
+--- src/System.Management.Automation/engine/PSVersionInfo.cs.orig	2024-04-11 18:38:22 UTC
 +++ src/System.Management.Automation/engine/PSVersionInfo.cs
 @@ -85,14 +85,14 @@ namespace System.Management.Automation
          {
@@ -8,13 +8,14 @@
 +            /*s_psSemVersion = Version_Label == string.Empty
                  ? new SemanticVersion(Version_Major, Version_Minor, Version_Patch)
 -                : new SemanticVersion(Version_Major, Version_Minor, Version_Patch, Version_Label, buildLabel: null);
+-            s_psVersion = (Version)s_psSemVersion;
 +                : new SemanticVersion(Version_Major, Version_Minor, Version_Patch, Version_Label, buildLabel: null);*/
-             s_psVersion = (Version)s_psSemVersion;
++            s_psVersion = Version.Parse("7.2.4");
  
              s_psVersionTable[PSVersionName] = s_psSemVersion;
              s_psVersionTable[PSEditionName] = PSEditionValue;
 -            s_psVersionTable[PSGitCommitIdName] = GitCommitId;
-+            //s_psVersionTable[PSGitCommitIdName] = GitCommitId;
++            s_psVersionTable[PSGitCommitIdName] = 0;
              s_psVersionTable[PSCompatibleVersionsName] = new Version[] { s_psV1Version, s_psV2Version, s_psV3Version, s_psV4Version, s_psV5Version, s_psV51Version, s_psV6Version, s_psV7Version };
              s_psVersionTable[SerializationVersionName] = new Version(InternalSerializer.DefaultVersion);
              s_psVersionTable[PSRemotingProtocolVersionName] = RemotingConstants.ProtocolVersion;
