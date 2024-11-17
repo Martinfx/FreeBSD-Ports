05bec72a29d5bcdfdee2ceb761b4e6652954138a
M. <1999714+Martinfx@users.noreply.github.com>
2024-11-17 09:35:02 +0100

    zeno: fix in poudriere (#167)

--- zeno/files/patch-ui_zenoedit_util_apphelper.h
+++ zeno/files/patch-ui_zenoedit_util_apphelper.h
@@ -1,34 +1,31 @@
---- ui/zenoedit/util/apphelper.h.orig	2024-07-27 11:22:11 UTC
+--- ui/zenoedit/util/apphelper.h.orig	2024-09-30 13:46:54 UTC
 +++ ui/zenoedit/util/apphelper.h
-@@ -1,19 +1,18 @@
- #ifndef __ZENOEDIT_HELPER__
+@@ -2,18 +2,17 @@
  #define __ZENOEDIT_HELPER__
  
--#include <zenomodel/include/igraphsmodel.h>
+ #include <zenomodel/include/igraphsmodel.h>
 -#include "zenoapplication.h"
--#include <zenomodel/include/graphsmanagment.h>
--#include <zenomodel/include/igraphsmodel.h>
+ #include <zenomodel/include/graphsmanagment.h>
+ #include <zenomodel/include/igraphsmodel.h>
 -#include "zenomainwindow.h"
--#include <zenovis/ObjectsManager.h>
--#include <zeno/types/UserData.h>
+ #include <zenovis/ObjectsManager.h>
+ #include <zeno/types/UserData.h>
 -#include <zenoui/comctrl/gv/zveceditoritem.h>
 -#include <viewport/viewportwidget.h>
--#include "launch/corelaunch.h"
--#include "settings/zsettings.h"
--#include "viewport/recordvideomgr.h"
--#include "panel/zenospreadsheet.h"
-+ #include <zenomodel/include/igraphsmodel.h>
-+ #include <zenomodel/include/graphsmanagment.h>
-+ #include <zenomodel/include/igraphsmodel.h>
-+ #include <zenovis/ObjectsManager.h>
-+ #include <zeno/types/UserData.h>
-+ #include "launch/corelaunch.h"
-+ #include "settings/zsettings.h"
-+ #include "viewport/recordvideomgr.h"
-+ #include "panel/zenospreadsheet.h"
+ #include "launch/corelaunch.h"
+ #include "settings/zsettings.h"
+ #include "viewport/recordvideomgr.h"
+ #include "panel/zenospreadsheet.h"
 +
 +class ZVecEditorItem;
 +class ViewPortWidget;
  
  class AppHelper
  {
+@@ -39,4 +38,4 @@ class AppHelper (public)
+ };
+ 
+ 
+-#endif
+\ No newline at end of file
++#endif
