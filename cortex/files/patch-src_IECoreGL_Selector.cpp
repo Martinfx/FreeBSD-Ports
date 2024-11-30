b32c25780bed83c83d931e3eac92fd67e47fd110
maxfx <maficccc@gmail.com>
2024-11-30 19:59:41 +0100

    cortex: new port

---
+++ cortex/files/patch-src_IECoreGL_Selector.cpp
@@ -0,0 +1,11 @@
+--- src/IECoreGL/Selector.cpp.orig	2024-11-29 22:47:43 UTC
++++ src/IECoreGL/Selector.cpp
+@@ -53,7 +53,7 @@
+ #include "IECore/MessageHandler.h"
+ 
+ #include "boost/format.hpp"
+-#include "boost/timer.hpp"
++#include "boost/timer/timer.hpp"
+ 
+ using namespace IECoreGL;
+ 
