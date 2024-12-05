6c8b1c657ab778c996d590958d95bbdec8210c12
maxfx <maficccc@gmail.com>
2024-12-05 15:29:19 +0100

    cortex: new port

--- gaffer/files/patch-src_Gaffer_Metadata.cpp
+++ gaffer/files/patch-src_Gaffer_Metadata.cpp
@@ -1,23 +1,27 @@
---- src/Gaffer/Metadata.cpp.orig	2024-12-04 08:23:08 UTC
+--- src/Gaffer/Metadata.cpp.orig	2024-11-22 14:42:33 UTC
 +++ src/Gaffer/Metadata.cpp
-@@ -50,12 +50,14 @@
+@@ -50,16 +50,18 @@
  #include "boost/multi_index/sequenced_index.hpp"
  #include "boost/multi_index_container.hpp"
  
 -#include "tbb/concurrent_hash_map.h"
 -#include "tbb/recursive_mutex.h"
 +#include "oneapi/tbb/concurrent_hash_map.h"
-+//#include "oneapi/tbb/mutex.h"
  
  #include <unordered_map>
-+#include <mutex>
 +#include <memory>
 +#include <vector>
++#include <mutex>
  
 -using namespace std;
  using namespace boost;
  using namespace tbb;
  using namespace IECore;
+ using namespace Gaffer;
++using namespace std;
+ 
+ //////////////////////////////////////////////////////////////////////////
+ // Internal implementation details
 @@ -86,8 +88,8 @@ struct NodeSignals
  	Metadata::PlugValueChangedSignal plugSignal;
  };
@@ -25,18 +29,21 @@
 -using SignalsMap = std::unordered_map<Node *, unique_ptr<NodeSignals>>;
 -using SignalsMapLock = tbb::recursive_mutex::scoped_lock;
 +using SignalsMap = std::unordered_map<Node *, std::unique_ptr<NodeSignals>>;
-+using SignalsMapLock = std::scoped_lock;
++using SignalsMapLock = std::unique_lock<std::recursive_mutex>;
  
  // Access to the signals requires the passing of a scoped_lock that
  // will be locked for you automatically, and must remain locked while
-@@ -95,8 +97,8 @@ SignalsMap &signalsMap( SignalsMapLock &lock )
+@@ -95,8 +97,11 @@ SignalsMap &signalsMap( SignalsMapLock &lock )
  SignalsMap &signalsMap( SignalsMapLock &lock )
  {
  	static SignalsMap *g_signalsMap = new SignalsMap;
 -	static tbb::recursive_mutex g_signalsMapMutex;
 -	lock.acquire( g_signalsMapMutex );
-+    static std::recursive_mutex g_signalsMapMutex;
-+    lock.lock( g_signalsMapMutex );
++	static std::recursive_mutex g_signalsMapMutex;
++    	if (!lock.owns_lock())
++    	{
++        	lock = SignalsMapLock(g_signalsMapMutex);
++	}
  	return *g_signalsMap;
  }
  
