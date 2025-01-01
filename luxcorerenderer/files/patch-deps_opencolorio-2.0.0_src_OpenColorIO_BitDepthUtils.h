e8dbe1b1751e405642bf1a7ea9f31c44db95aa1e
maxfx <maficccc@gmail.com>
2025-01-01 20:58:14 +0100

    Lux render: new port

---
+++ luxcorerenderer/files/patch-deps_opencolorio-2.0.0_src_OpenColorIO_BitDepthUtils.h
@@ -0,0 +1,11 @@
+--- deps/opencolorio-2.0.0/src/OpenColorIO/BitDepthUtils.h.orig	2025-01-01 14:32:20 UTC
++++ deps/opencolorio-2.0.0/src/OpenColorIO/BitDepthUtils.h
+@@ -7,7 +7,7 @@
+ 
+ #include <OpenColorIO/OpenColorIO.h>
+ 
+-#include "OpenEXR/half.h"
++#include <Imath/half.h>
+ 
+ 
+ namespace OCIO_NAMESPACE
