9548b2329a769597c533221b7a791e6c932a82c2
maxfx <maficccc@gmail.com>
2025-01-01 15:27:53 +0100

    Lux render: new port

---
+++ luxcorerenderer/files/patch-deps_opencolorio-2.0.0_src_OpenColorIO_MathUtils.h
@@ -0,0 +1,11 @@
+--- deps/opencolorio-2.0.0/src/OpenColorIO/MathUtils.h.orig	2025-01-01 13:45:31 UTC
++++ deps/opencolorio-2.0.0/src/OpenColorIO/MathUtils.h
+@@ -9,7 +9,7 @@
+ 
+ #include <OpenColorIO/OpenColorIO.h>
+ 
+-#include "OpenEXR/half.h"
++#include <Imath/half.h>
+ 
+ namespace OCIO_NAMESPACE
+ {
