5f8237efbc9c66b8e3aca447527e32e665c71a6d
M. <1999714+Martinfx@users.noreply.github.com>
2025-03-01 11:04:09 +0100

    powershell: update to 7.5.0-rc.1 (#190)
    * powershell: update 7.5.0-rc.1

--- powershell/files/patch-src_Microsoft.PowerShell.ConsoleHost_host_msh_ConsoleHost.cs
+++ powershell/files/patch-src_Microsoft.PowerShell.ConsoleHost_host_msh_ConsoleHost.cs
@@ -1,6 +1,6 @@
---- src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs.orig	2024-09-21 21:25:26 UTC
+--- src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs.orig	2024-11-14 22:14:58 UTC
 +++ src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs
-@@ -181,7 +181,7 @@ namespace Microsoft.PowerShell
+@@ -193,7 +193,7 @@ namespace Microsoft.PowerShell
                      // Alternatively, we could call s_theConsoleHost.UI.WriteLine(s_theConsoleHost.Version.ToString());
                      // or start up the engine and retrieve the information via $psversiontable.GitCommitId
                      // but this returns the semantic version and avoids executing a script
