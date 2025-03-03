4ad10b15291538d125b32b5e976fe7b8796830f3
MartinFx <maficccc@gmail.com>
2025-03-03 13:33:50 +0100

    gaffer: new port

--- gaffer/files/patch-include_GafferImageUI_ImageGadget.h
+++ gaffer/files/patch-include_GafferImageUI_ImageGadget.h
@@ -1,11 +1,23 @@
 --- include/GafferImageUI/ImageGadget.h.orig	2025-03-03 10:27:42 UTC
 +++ include/GafferImageUI/ImageGadget.h
-@@ -294,7 +294,7 @@ class GAFFERIMAGEUI_API ImageGadget : public GafferUI:
+@@ -294,17 +294,17 @@ class GAFFERIMAGEUI_API ImageGadget : public GafferUI:
  			void resetActive();
  
  			// Called from the UI thread.
 -			const IECoreGL::Texture *texture( bool &active );
-+			IECoreGL::Texture *texture( bool &active );
++			IECoreGL::Texture *texture( bool &active ) const;
  
  			private :
+ 
+ 				IECore::MurmurHash m_channelDataHash;
+ 				IECore::ConstFloatVectorDataPtr m_channelDataToConvert;
+-				IECoreGL::TexturePtr m_texture;
++				mutable IECoreGL::TexturePtr m_texture;
+ 				bool m_active;
+ 				std::chrono::steady_clock::time_point m_activeStartTime;
+ 				using Mutex = tbb::spin_mutex;
+-				Mutex m_mutex;
++				mutable Mutex m_mutex;
+ 
+ 		};
  
--- gaffer/files/patch-src_GafferImageUI_ImageGadget.cpp
+++ gaffer/files/patch-src_GafferImageUI_ImageGadget.cpp
@@ -5,7 +5,25 @@
  }
  
 -const IECoreGL::Texture *ImageGadget::Tile::texture( bool &active )
-+IECoreGL::Texture *ImageGadget::Tile::texture( bool &active )
++IECoreGL::Texture *ImageGadget::Tile::texture( bool &active ) const
  {
  	const auto now = std::chrono::steady_clock::now();
  	Mutex::scoped_lock lock( m_mutex );
+@@ -770,7 +770,7 @@ const IECoreGL::Texture *ImageGadget::Tile::texture( b
+ 	}
+ 
+ 	ConstFloatVectorDataPtr channelDataToConvert = m_channelDataToConvert;
+-	m_channelDataToConvert = nullptr;
++	
+ 	lock.release(); // Don't hold lock while doing expensive conversion
+ 
+ 	if( channelDataToConvert )
+@@ -781,7 +781,7 @@ const IECoreGL::Texture *ImageGadget::Tile::texture( b
+ 		{
+ 			GLuint texture;
+ 			glGenTextures( 1, &texture );
+-			m_texture = new Texture( texture ); // Lock not needed, because this is only touched on the UI thread.
++			m_texture = new Texture(texture); // Lock not needed, because this is only touched on the UI thread.
+ 			Texture::ScopedBinding binding( *m_texture );
+ 
+ 			glTexImage2D(
