432ea133196eaae1f1bd3cd05d152b8af5621ff1
MartinFx <maficccc@gmail.com>
2025-03-04 17:03:02 +0100

    cycles: new port

--- cycles/files/patch-src_util_atomic.h
+++ cycles/files/patch-src_util_atomic.h
@@ -1,11 +1,11 @@
---- src/util/atomic.h.orig	2025-03-04 14:43:48 UTC
+--- src/util/atomic.h.orig	2025-03-04 16:01:33 UTC
 +++ src/util/atomic.h
-@@ -21,7 +21,7 @@
- #  define ccl_barrier(flags) ((void)0)
+@@ -5,6 +5,8 @@
+ #ifndef __UTIL_ATOMIC_H__
+ #define __UTIL_ATOMIC_H__
  
- #else /* __KERNEL_GPU__ */
--
-+#  include "atomic_ops.h"
- #  ifndef __KERNEL_ONEAPI__
- #    define atomic_fetch_and_add_uint32_shared atomic_fetch_and_add_uint32
- #  endif
++#include "atomic_ops.h"
++
+ #ifndef __KERNEL_GPU__
+ 
+ /* Using atomic ops header from Blender. */
