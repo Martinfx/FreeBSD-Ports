d861642800ff895a81a2c5668c33bb902c562eca
MartinFx <maficccc@gmail.com>
2025-03-07 11:58:09 +0100

    gaffer: new port

---
+++ gaffer/files/patch-src_GafferUSD_USDShader.cpp
@@ -0,0 +1,11 @@
+--- src/GafferUSD/USDShader.cpp.orig	2025-03-07 10:52:48 UTC
++++ src/GafferUSD/USDShader.cpp
+@@ -301,7 +301,7 @@ Plug *loadShaderProperty( const SdrShaderProperty &pro
+ 
+ Plug *loadShaderProperty( const SdrShaderProperty &property, Plug *parent )
+ {
+-	SdfValueTypeName sdfType = property.GetTypeAsSdfType().first;
++	SdfValueTypeName sdfType = property.GetTypeAsSdfType();
+ 	if(
+ 		property.GetType() == SdrPropertyTypes->Terminal ||
+ 		property.GetType() == SdrPropertyTypes->Vstruct
