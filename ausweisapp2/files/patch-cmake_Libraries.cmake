db9254535fe15082bdf3807a1fe78161367f2aab
M. <1999714+Martinfx@users.noreply.github.com>
2025-03-10 08:47:19 +0100

    ausweisapp2: update to 2.3.0 (#221)
    * ausweisapp2: update to 2.3.0

--- ausweisapp2/files/patch-cmake_Libraries.cmake
+++ ausweisapp2/files/patch-cmake_Libraries.cmake
@@ -1,17 +1,24 @@
---- cmake/Libraries.cmake.orig	2021-09-16 18:07:00 UTC
+--- cmake/Libraries.cmake.orig	2024-04-10 11:44:19 UTC
 +++ cmake/Libraries.cmake
-@@ -122,11 +122,9 @@ elseif(MAC)
- 	find_library(OSX_FOUNDATION Foundation)
- 	find_library(OSX_SERVICEMANAGEMENT ServiceManagement)
- elseif(UNIX)
--	if(LINUX)
--		find_library(LIBUDEV NAMES udev ludev libudev)
--		if(NOT LIBUDEV)
--			message(STATUS "Hardware detection disabled - Missing libudev")
--		endif()
-+	find_library(LIBUDEV NAMES udev ludev libudev)
-+	if(NOT LIBUDEV)
-+		message(STATUS "Hardware detection disabled - Missing libudev")
- 	endif()
+@@ -147,12 +147,12 @@ if(QT_COMPONENTS)
+ 	find_package(${Qt} ${MIN_QT_VERSION} COMPONENTS ${QT_COMPONENTS} REQUIRED)
+ endif()
  
- 	find_package(PkgConfig REQUIRED)
+-if(LINUX OR BSD)
+-	try_run(testResult compileResult "${CMAKE_BINARY_DIR}" "${CMAKE_DIR}/tests/openssl.cpp"
+-		CMAKE_FLAGS INSTALL_RPATH_USE_LINK_PATH:BOOL=ON
+-		LINK_LIBRARIES ${Qt}::Network OpenSSL::Crypto OpenSSL::SSL
+-		OUTPUT_VARIABLE runResult)
+-	if(NOT testResult EQUAL 0)
+-		message(FATAL_ERROR "Your OpenSSL library looks incompatible: ${testResult}\n${runResult}")
+-	endif()
+-endif()
++#if(LINUX OR BSD)
++#	try_run(testResult compileResult "${CMAKE_BINARY_DIR}" "${CMAKE_DIR}/tests/openssl.cpp"
++#		CMAKE_FLAGS INSTALL_RPATH_USE_LINK_PATH:BOOL=ON
++#		LINK_LIBRARIES ${Qt}::Network OpenSSL::Crypto OpenSSL::SSL
++#		OUTPUT_VARIABLE runResult)
++#	if(NOT testResult EQUAL 0)
++#		message(FATAL_ERROR "Your OpenSSL library looks incompatible: ${testResult}\n${runResult}")
++#	endif()
++#endif()
