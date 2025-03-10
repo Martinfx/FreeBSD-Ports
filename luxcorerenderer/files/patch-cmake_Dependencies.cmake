eddf1f489fe7f273f5f5f7bab3a8ba059742ffcb
MartinFx <maficccc@gmail.com>
2025-03-10 09:10:38 +0100

    luxrender: update to 2.9alpha4

--- luxcorerenderer/files/patch-cmake_Dependencies.cmake
+++ luxcorerenderer/files/patch-cmake_Dependencies.cmake
@@ -1,11 +1,11 @@
---- cmake/Dependencies.cmake.orig	2025-01-08 07:43:39 UTC
+--- cmake/Dependencies.cmake.orig	2025-01-10 06:16:26 UTC
 +++ cmake/Dependencies.cmake
 @@ -103,12 +103,13 @@ set(Boost_MINIMUM_VERSION       "1.85.0") #1.56.0 1.85
  set(Boost_DEBUG					OFF)
  # set(Boost_NO_BOOST_CMAKE 		ON)
  set(Boost_MINIMUM_VERSION       "1.85.0") #1.56.0 1.85.0
 -set(Boost_VERSION       		"1.85.0")
-+set(Boost_VERSION       		"1.86.0")
++set(Boost_VERSION       		"1.87.0")
  message(STATUS "Boost version: ${Boost_VERSION}")
  
  # For Windows builds, PYTHON_V must be defined as "3x" (x=Python minor version, e.g. "35")
@@ -21,7 +21,7 @@
  # OpencolorIO
  set(OpenColorIO_INCLUDE_DIR "/usr/include/OpenColorIO")
 -set(OpenColorIO_LIBRARY "/usr/lib64/libOpenColorIO.so")
-+set(OpenColorIO_LIBRARY "/usr/local/lib/libOpenColorIO.so")
++set(OpenColorIO_LIBRARY "-lOpenColorIO")
  find_package(OpenColorIO REQUIRED)
  include_directories(${OpenColorIO_INCLUDE_DIRS})
  
