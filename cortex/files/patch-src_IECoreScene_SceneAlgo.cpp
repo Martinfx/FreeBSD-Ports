62d79d3104a4dd2af8a0a418b0c4c01b6436d797
MartinFx <maficccc@gmail.com>
2025-03-11 14:27:45 +0100

    cortex: new port without usd

--- cortex/files/patch-src_IECoreScene_SceneAlgo.cpp
+++ cortex/files/patch-src_IECoreScene_SceneAlgo.cpp
@@ -1,22 +1,15 @@
---- src/IECoreScene/SceneAlgo.cpp.orig	2024-12-04 19:36:02 UTC
+--- src/IECoreScene/SceneAlgo.cpp.orig	2025-03-11 12:19:05 UTC
 +++ src/IECoreScene/SceneAlgo.cpp
-@@ -1,4 +1,4 @@
--//////////////////////////////////////////////////////////////////////////
-+////////////////////////////////////////////////////////////////////////////
- //
- //  Copyright (c) 2018, Image Engine Design Inc. All rights reserved.
- //
-@@ -39,7 +39,8 @@
+@@ -39,7 +39,7 @@
  #include "IECoreScene/PointsPrimitive.h"
  #include "IECoreScene/SceneInterface.h"
  
 -#include "tbb/task.h"
-+//#include "tbb/task.h"
-+#include "oneapi/tbb/task_group.h"
++#include <oneapi/tbb/task_group.h>
  
  #include <atomic>
  
-@@ -50,7 +51,7 @@ template<typename LocationFn>
+@@ -50,7 +50,7 @@ template<typename LocationFn>
  {
  
  template<typename LocationFn>
@@ -25,52 +18,100 @@
  {
  
  	public :
-@@ -64,7 +65,7 @@ class Task : public tbb::task
- 		~Task() override
+@@ -61,44 +61,41 @@ class Task : public tbb::task
  		{
  		}
--
-+/*
- 		task *execute() override
+ 
+-		~Task() override
++		~Task()
  		{
- 			m_locationFn( m_src, m_dst, m_time, m_flags );
-@@ -97,7 +98,7 @@ class Task : public tbb::task
- 			wait_for_all();
+ 		}
  
- 			return nullptr;
+-		task *execute() override
+-		{
+-			m_locationFn( m_src, m_dst, m_time, m_flags );
++        	void operator()() const
++        	{
++            		m_locationFn(m_src, m_dst, m_time, m_flags);
+ 
+-			SceneInterface::NameList childNames;
+-			m_src->childNames( childNames );
++            		SceneInterface::NameList childNames;
++            		m_src->childNames(childNames);
+ 
+-			set_ref_count( 1 + childNames.size() );
++            		std::vector<SceneInterfacePtr> childSceneInterfaces;
++            		childSceneInterfaces.reserve(childNames.size());
+ 
+-			std::vector<SceneInterfacePtr> childSceneInterfaces;
+-			childSceneInterfaces.reserve( childNames.size() );
++            		std::vector<ConstSceneInterfacePtr> srcChildSceneInterfaces;
++            		srcChildSceneInterfaces.reserve(childNames.size());
+ 
+-			std::vector<ConstSceneInterfacePtr> srcChildSceneInterfaces;
+-			srcChildSceneInterfaces.reserve( childNames.size() );
++            		oneapi::tbb::task_group tg;
++            		for (const auto& childName : childNames)
++            		{
++                		SceneInterfacePtr dstChild = m_dst ? m_dst->child(childName, SceneInterface::CreateIfMissing) : nullptr;
++                		if (dstChild)
++                		{
++                    			childSceneInterfaces.push_back(dstChild);
++                	}
+ 
+-			for( const auto &childName : childNames )
+-			{
+-				SceneInterfacePtr dstChild = m_dst ? m_dst->child( childName, SceneInterface::CreateIfMissing ) : nullptr;
+-				if( dstChild )
+-				{
+-					childSceneInterfaces.push_back( dstChild );
+-				}
++                	ConstSceneInterfacePtr srcChild = m_src->child(childName);
++                	srcChildSceneInterfaces.push_back(srcChild);
+ 
+-				ConstSceneInterfacePtr srcChild = m_src->child( childName );
+-				srcChildSceneInterfaces.push_back( srcChild );
++                	tg.run(Task(srcChild.get(), dstChild.get(), m_locationFn, m_time, m_flags));
++            	}
+ 
+-				Task *t = new( allocate_child() ) Task( srcChild.get(), dstChild.get(), m_locationFn, m_time, m_flags );
+-				spawn( *t );
+-			}
+-			wait_for_all();
++            	tg.wait();
++        }
+ 
+-			return nullptr;
 -		}
-+		}*/
- 
+-
  	private :
  
-@@ -257,13 +258,23 @@ SceneStats parallelReadAll( const SceneInterface *src,
+ 		const SceneInterface *m_src;
+@@ -257,12 +254,18 @@ SceneStats parallelReadAll( const SceneInterface *src,
  		copyInfos.pointCount += copyInfo.pointCount;
  	};
  
-+	   // Create a TBB task group for parallel processing
-+/*    	oneapi::tbb::task_group taskGroup;
- 	for( int f = startFrame; f <= endFrame; ++f )
+-	for( int f = startFrame; f <= endFrame; ++f )
++	for (int f = startFrame; f <= endFrame; ++f)
  	{
- 		double time = f / frameRate;
+-		double time = f / frameRate;
 -		tbb::task_group_context taskGroupContext( tbb::task_group_context::isolated );
 -		Task<decltype( locationFn )> *task = new( tbb::task::allocate_root( taskGroupContext ) ) Task<decltype( locationFn )>( src, nullptr, locationFn, time, flags );
 -		tbb::task::spawn_root_and_wait( *task );
-+		//oneapi::tbb::task_group_context taskGroupContext( oneapi::tbb::task_group_context::isolated );
-+		//Task<decltype( locationFn )> *task = new( taskGroupContext ) Task<decltype( locationFn )>( src, nullptr, locationFn, time, flags );
-+		//oneapi::tbb::task::spawn_root_and_wait( *task );
-+		taskGroup.run([=, &locationFn]() {
-+           	// Create and execute a task for each frame
-+           	Task<decltype(locationFn)> task(src, locationFn, time, flags);
-+            	task.execute();
-+        	});
- 	}
++    	double time = f / frameRate;
++  	  	oneapi::tbb::task_group_context taskGroupContext(oneapi::tbb::task_group_context::isolated);
++    	oneapi::tbb::task_group tg(taskGroupContext);
 +
-+	// Wait for all tasks to complete
-+    	taskGroup.wait();*/
++    	tg.run([=, &locationFn]() {
++        	Task<decltype(locationFn)> task(src, nullptr, locationFn, time, flags);
++       	 	task();
++    	});
++
++    	tg.wait();
+ 	}
  
  	SceneStats stats;
- 	stats["locations"] = locationCount;
-@@ -293,4 +304,4 @@ void copy( const SceneInterface *src, SceneInterface *
+@@ -293,4 +296,4 @@ void copy( const SceneInterface *src, SceneInterface *
  
  } // SceneAlgo
  
