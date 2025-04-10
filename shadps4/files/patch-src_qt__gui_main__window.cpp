2e302e4189a14403dd1d00fc3fe0cbaafa5f0c02
MartinFx <maficccc@gmail.com>
2025-04-10 17:36:29 +0200

    shadps4: new port

--- shadps4/files/patch-src_qt__gui_main__window.cpp
+++ shadps4/files/patch-src_qt__gui_main__window.cpp
@@ -1,4 +1,4 @@
---- src/qt_gui/main_window.cpp.orig	2025-04-08 06:31:06 UTC
+--- src/qt_gui/main_window.cpp.orig	2025-03-27 20:40:40 UTC
 +++ src/qt_gui/main_window.cpp
 @@ -253,6 +253,8 @@ void MainWindow::UpdateToolbarButtons() {
  }
@@ -9,3 +9,12 @@
      // add toolbar widgets when game is running
      bool showLabels = ui->toggleLabelsAct->isChecked();
  
+@@ -1484,7 +1486,7 @@ void MainWindow::StartEmulator(std::filesystem::path p
+         return;
+     }
+     isGameRunning = true;
+-#ifdef __APPLE__
++#if defined( __APPLE__) || defined(__FreeBSD__)
+     // SDL on macOS requires main thread.
+     Core::Emulator emulator;
+     emulator.Run(path);
