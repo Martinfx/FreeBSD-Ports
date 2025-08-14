a0674bfbd4150809402fb472c5f8028c66075529
MartinFx <maficccc@gmail.com>
2025-08-14 08:26:57 +0200

    compute-library: next work

--- computelibrary/files/patch-src_common_cpuinfo_CpuInfo.cpp
+++ computelibrary/files/patch-src_common_cpuinfo_CpuInfo.cpp
@@ -1,6 +1,15 @@
 --- src/common/cpuinfo/CpuInfo.cpp.orig	2025-07-01 09:33:23 UTC
 +++ src/common/cpuinfo/CpuInfo.cpp
-@@ -70,7 +70,7 @@ namespace
+@@ -31,6 +31,8 @@
+ 
+ #include <map>
+ #include <sstream>
++#include <sys/types.h>
++#include <sys/sysctl.h>
+ 
+ #if !defined(BARE_METAL)
+ #include <algorithm>
+@@ -70,7 +72,7 @@ namespace
  {
  namespace
  {
@@ -9,7 +18,7 @@
      (defined(__arm__) || defined(__aarch64__))
  /** Extract MIDR using CPUID information that are exposed to user-space
   *
-@@ -352,7 +352,7 @@ CpuInfo CpuInfo::build()
+@@ -352,7 +354,7 @@ CpuInfo CpuInfo::build()
  
  CpuInfo CpuInfo::build()
  {
@@ -18,16 +27,25 @@
      (defined(__arm__) || defined(__aarch64__))
      const uint32_t hwcaps   = getauxval(AT_HWCAP);
      const uint32_t hwcaps2  = getauxval(AT_HWCAP2);
-@@ -383,7 +383,7 @@ CpuInfo CpuInfo::build()
+@@ -383,7 +385,7 @@ CpuInfo CpuInfo::build()
  
      CpuInfo info(isa, cpus_model);
      return info;
 -#elif defined(__OpenBSD__)
-+#elif defined(__OpenBSD__) || defined(__FreeBSD__) 
++#elif defined(__OpenBSD__) || defined(__FreeBSD__)
      int    mib[2] = {0, 0};
      int    ncpu   = {1};
      size_t len    = sizeof(ncpu);
-@@ -466,7 +466,7 @@ CpuModel CpuInfo::cpu_model() const
+@@ -420,7 +422,7 @@ CpuInfo CpuInfo::build()
+     CpuInfo               info(isa, cpus_model);
+     return info;
+ #elif defined(__aarch64__) && \
+-    (defined(__OpenBSD__) || defined(__APPLE__)) /* #elif(BARE_METAL) && defined(__aarch64__) */
++    (defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__APPLE__)) /* #elif(BARE_METAL) && defined(__aarch64__) */
+     int                   ncpus = get_hw_capability("hw.perflevel0.logicalcpu");
+     CpuIsaInfo            isainfo;
+     std::vector<CpuModel> cpus_model(ncpus);
+@@ -466,7 +468,7 @@ CpuModel CpuInfo::cpu_model() const
  
  CpuModel CpuInfo::cpu_model() const
  {
