3942a93aa5a766662b46ffc7a6fe8584187d6538
MartinFx <maficccc@gmail.com>
2025-08-30 00:23:18 +0200

    waterfox: update to 6.6.2

--- waterfox/files/patch-toolkit_xre_nsEmbedFunctions.cpp
+++ waterfox/files/patch-toolkit_xre_nsEmbedFunctions.cpp
@@ -1,17 +1,19 @@
-diff --git toolkit/xre/nsEmbedFunctions.cpp toolkit/xre/nsEmbedFunctions.cpp
-index 9ce6af94637c..44165558f7ec 100644
---- toolkit/xre/nsEmbedFunctions.cpp
+--- toolkit/xre/nsEmbedFunctions.cpp.orig	2025-08-28 19:08:14 UTC
 +++ toolkit/xre/nsEmbedFunctions.cpp
-@@ -360,10 +360,12 @@ nsresult XRE_InitChildProcess(int aArgc, char* aArgv[],
-           static_cast<CrashReporter::ProcessId>(*crashHelperPidArg);
- #endif  // defined(XP_LINUX) && !defined(MOZ_WIDGET_ANDROID)
+@@ -348,6 +348,7 @@ nsresult XRE_InitChildProcess(int aArgc, char* aArgv[]
+ 
+   SetupErrorHandling(aArgv[0]);
  
 +#if !defined(XP_FREEBSD)
-       exceptionHandlerIsSet = CrashReporter::SetRemoteExceptionHandler(
-           std::move(*crashReporterArg), crashHelperPid);
+   bool exceptionHandlerIsSet = false;
+   if (!CrashReporter::IsDummy()) {
+     auto crashReporterArg = geckoargs::sCrashReporter.Get(aArgc, aArgv);
+@@ -357,7 +358,7 @@ nsresult XRE_InitChildProcess(int aArgc, char* aArgv[]
+           std::move(*crashReporterArg), std::move(*crashHelperArg));
        MOZ_ASSERT(exceptionHandlerIsSet,
                   "Should have been able to set remote exception handler");
+-
 +#endif
- 
        if (!exceptionHandlerIsSet) {
          // Bug 684322 will add better visibility into this condition
+         NS_WARNING("Could not setup crash reporting\n");
