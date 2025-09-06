d00b7fef4e620b42217c69de071acde3cd4e6cd6
MartinFx <maficccc@gmail.com>
2025-09-06 13:24:27 +0200

    qt60ads: new port

--- pyside6-qtads/files/patch-scripts_pyside__config.py
+++ pyside6-qtads/files/patch-scripts_pyside__config.py
@@ -1,10 +1,10 @@
---- scripts/pyside_config.py.orig	2025-09-06 10:46:39 UTC
+--- scripts/pyside_config.py.orig	2025-09-02 06:36:29 UTC
 +++ scripts/pyside_config.py
 @@ -155,6 +155,8 @@ def link_option(lib):
      link = ' -l'
      if sys.platform in ['linux', 'linux2']: # Linux: 'libfoo.so' -> '/absolute/path/libfoo.so'
          link = lib
-+    elif sys.platform in ['freebsd']: # Linux: 'libfoo.so' -> '/absolute/path/libfoo.so'
++    elif sys.platform in ['freebsd','freebsd14']: 
 +        link = lib
      elif sys.platform in ['darwin']: # Darwin: 'libfoo.so' -> '-lfoo'
          link += os.path.splitext(baseName[3:])[0]
