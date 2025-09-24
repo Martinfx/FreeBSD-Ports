25da489139fbdc7aac95122e0e5ad87bcc9c54e5
Martin Filla <freebsd@sysctl.cz>
2025-09-24 13:55:37 +0200

    onluoffice: new port

--- onlyoffice/files/patch-core_Common_3dParty_html_katana-parser_src_parser.c
+++ onlyoffice/files/patch-core_Common_3dParty_html_katana-parser_src_parser.c
@@ -1,21 +1,11 @@
---- core/Common/3dParty/html/katana-parser/src/parser.c.orig	2025-09-24 11:15:53 UTC
+--- core/Common/3dParty/html/katana-parser/src/parser.c.orig	2025-09-24 11:53:01 UTC
 +++ core/Common/3dParty/html/katana-parser/src/parser.c
-@@ -1275,7 +1275,7 @@ void katanaerror(YYLTYPE* yyloc, void* scanner, struct
-            yyloc->last_line,
-            yyloc->last_column,
-            error,
--           /*/*katanaget_text(parser->scanner)*/"error"*/"error");
-+           /*/*/*katanaget_text(parser->scanner)*/"error"*/"error"*/"error");
- 
-     YYSTYPE * s = katanaget_lval(parser->scanner);
- 
-@@ -1292,8 +1292,7 @@ void katanaerror(YYLTYPE* yyloc, void* scanner, struct
-     e->first_column = yyloc->first_column;
+@@ -1293,7 +1293,7 @@ void katanaerror(YYLTYPE* yyloc, void* scanner, struct
      e->last_line = yyloc->last_line;
      e->last_column = yyloc->last_column;
--    snprintf(e->message, KATANA_ERROR_MESSAGE_SIZE, "%s at %s", error,
--             /*/*katanaget_text(parser->scanner)*/"error"*/"error");
-+    snprintf(e->message, KATANA_ERROR_MESSAGE_SIZE, "%s at %s", error,"error");
+     snprintf(e->message, KATANA_ERROR_MESSAGE_SIZE, "%s at %s", error,
+-             katanaget_text(parser->scanner));
++             	katanaget_text(parser->scanner));
      katana_array_add(parser, e, &(parser->output->errors));
  }
  
