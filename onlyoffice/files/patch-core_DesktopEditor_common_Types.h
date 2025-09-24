387162a68ae5d5b57a44966c9c948adac8ab2b0c
Martin Filla <freebsd@sysctl.cz>
2025-09-24 12:54:11 +0200

    onluoffice: new port

--- onlyoffice/files/patch-core_DesktopEditor_common_Types.h
+++ onlyoffice/files/patch-core_DesktopEditor_common_Types.h
@@ -1,11 +1,11 @@
---- core/DesktopEditor/common/Types.h.orig	2025-09-23 23:07:02 UTC
+--- core/DesktopEditor/common/Types.h.orig	2025-09-24 10:40:53 UTC
 +++ core/DesktopEditor/common/Types.h
 @@ -78,7 +78,7 @@ typedef wchar_t			WCHAR;
  typedef unsigned int	UINT, *PUINT;
  typedef wchar_t			WCHAR;
  
 -#ifdef __linux__
-+#ifdef __linux__ || __FreeBSD__
++#if defined(__linux__) || defined(__FreeBSD__)
  #include <inttypes.h>
  typedef int64_t     T_LONG64;
  typedef uint64_t    T_ULONG64;
