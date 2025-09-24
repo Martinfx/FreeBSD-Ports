20f58aac29b6d97a9e2361f7b7f8788e171c67d9
Martin Filla <freebsd@sysctl.cz>
2025-09-24 08:23:55 +0200

    onluoffice: new port

--- onlyoffice/files/patch-core_DesktopEditor_xml_libxml2_xmlIO.c
+++ onlyoffice/files/patch-core_DesktopEditor_xml_libxml2_xmlIO.c
@@ -1,4 +1,4 @@
---- core/DesktopEditor/xml/libxml2/xmlIO.c.orig	2021-12-24 14:29:10 UTC
+--- core/DesktopEditor/xml/libxml2/xmlIO.c.orig	2025-09-24 06:22:37 UTC
 +++ core/DesktopEditor/xml/libxml2/xmlIO.c
 @@ -43,7 +43,7 @@
  #include <windows.h>
