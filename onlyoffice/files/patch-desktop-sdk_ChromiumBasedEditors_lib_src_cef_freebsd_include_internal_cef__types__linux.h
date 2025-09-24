df11bdbb8207dcc3cbc625a989206a16e6e412c1
Martin Filla <freebsd@sysctl.cz>
2025-09-24 20:50:12 +0200

    onluoffice: new port

--- onlyoffice/files/patch-desktop-sdk_ChromiumBasedEditors_lib_src_cef_freebsd_include_internal_cef__types__linux.h
+++ onlyoffice/files/patch-desktop-sdk_ChromiumBasedEditors_lib_src_cef_freebsd_include_internal_cef__types__linux.h
@@ -1,4 +1,4 @@
---- desktop-sdk/ChromiumBasedEditors/lib/src/cef/freebsd/include/internal/cef_types_linux.h.orig	2025-09-24 16:38:13 UTC
+--- desktop-sdk/ChromiumBasedEditors/lib/src/cef/freebsd/include/internal/cef_types_linux.h.orig	2025-09-24 18:11:29 UTC
 +++ desktop-sdk/ChromiumBasedEditors/lib/src/cef/freebsd/include/internal/cef_types_linux.h
 @@ -34,7 +34,7 @@
  #include "include/base/cef_build.h"
@@ -9,3 +9,12 @@
  
  #if defined(CEF_X11)
  typedef union _XEvent XEvent;
+@@ -47,7 +47,7 @@ typedef struct _XDisplay XDisplay;
+ 
+ // Handle types.
+ #if defined(CEF_X11)
+-#define cef_cursor_handle_t unsigned long
++#define cef_cursor_handle_t int
+ #define cef_event_handle_t XEvent*
+ #else
+ #define cef_cursor_handle_t void*
