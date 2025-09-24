c2fe8e9d67e0f39b59909b2728be3f4f5de67cbd
Martin Filla <freebsd@sysctl.cz>
2025-09-24 13:12:40 +0200

    onluoffice: new port

--- onlyoffice/files/patch-core_DesktopEditor_agg-2.4_include_agg__span__gradient.h
+++ onlyoffice/files/patch-core_DesktopEditor_agg-2.4_include_agg__span__gradient.h
@@ -1,11 +1,11 @@
---- core/DesktopEditor/agg-2.4/include/agg_span_gradient.h.orig	2025-09-23 22:58:36 UTC
+--- core/DesktopEditor/agg-2.4/include/agg_span_gradient.h.orig	2025-09-24 10:55:09 UTC
 +++ core/DesktopEditor/agg-2.4/include/agg_span_gradient.h
 @@ -20,7 +20,7 @@ namespace agg
  namespace agg
  {
      
 -#if !defined(_LINUX) && !(defined(_WIN32) || defined (_WIN64)) && !defined(__APPLE__)
-+#if !defined(_LINUX) && !(defined(_WIN32) && !defined(__FreeBSD__) || defined (_WIN64)) && !defined(__APPLE__)
++#if !defined(_LINUX) && !defined(__FreeBSD__) && !(defined(_WIN32) || defined (_WIN64)) && !defined(__APPLE__)
      double _hypot(double x, double y)
      {
          return sqrt(x*x + y*y);
