a6c8588e3e99790693b413968e59b32e555320b5
Martin Filla <freebsd@sysctl.cz>
2025-09-24 13:17:57 +0200

    onluoffice: new port

---
+++ onlyoffice/files/patch-core_Common_3dParty_html_katana-parser_src_parser.c
@@ -0,0 +1,12 @@
+--- core/Common/3dParty/html/katana-parser/src/parser.c.orig	2025-09-24 11:15:53 UTC
++++ core/Common/3dParty/html/katana-parser/src/parser.c
+@@ -1292,8 +1292,7 @@ void katanaerror(YYLTYPE* yyloc, void* scanner, struct
+     e->first_column = yyloc->first_column;
+     e->last_line = yyloc->last_line;
+     e->last_column = yyloc->last_column;
+-    snprintf(e->message, KATANA_ERROR_MESSAGE_SIZE, "%s at %s", error,
+-             /*/*katanaget_text(parser->scanner)*/"error"*/"error");
++    snprintf(e->message, KATANA_ERROR_MESSAGE_SIZE, "%s at %s", error,"error");
+     katana_array_add(parser, e, &(parser->output->errors));
+ }
+ 
