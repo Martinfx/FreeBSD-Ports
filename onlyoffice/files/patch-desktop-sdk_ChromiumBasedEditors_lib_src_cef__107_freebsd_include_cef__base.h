c51614b173d31566f2214ff99b9cef0f4c4bdf7c
Martin Filla <freebsd@sysctl.cz>
2025-09-26 21:09:12 +0200

    onluoffice: new port

--- onlyoffice/files/patch-desktop-sdk_ChromiumBasedEditors_lib_src_cef__107_freebsd_include_cef__base.h
+++ onlyoffice/files/patch-desktop-sdk_ChromiumBasedEditors_lib_src_cef__107_freebsd_include_cef__base.h
@@ -1,11 +1,11 @@
---- desktop-sdk/ChromiumBasedEditors/lib/src/cef_107/freebsd/include/cef_base.h.orig	2025-09-24 18:03:36 UTC
+--- desktop-sdk/ChromiumBasedEditors/lib/src/cef_107/freebsd/include/cef_base.h.orig	2025-09-26 19:04:10 UTC
 +++ desktop-sdk/ChromiumBasedEditors/lib/src/cef_107/freebsd/include/cef_base.h
-@@ -45,6 +45,8 @@
- #include "include/internal/cef_mac.h"
- #elif defined(OS_LINUX)
+@@ -47,6 +47,8 @@
  #include "include/internal/cef_linux.h"
-+#elif defined(OS_FREEBSD)
-+#include "include/internal/cef_freebsd.h"
  #endif
  
++#include "include/internal/cef_freebsd.h"
++
+ ///
+ /// All ref-counted framework classes must extend this class.
  ///
