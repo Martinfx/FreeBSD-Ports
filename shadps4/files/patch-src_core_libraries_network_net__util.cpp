d0a77b774b73d48a33da036dd3b070b320586a39
Martin Filla <freebsd@sysctl.cz>
2025-09-27 09:52:07 +0200

    shadps4: update to 0.11

--- shadps4/files/patch-src_core_libraries_network_net__util.cpp
+++ shadps4/files/patch-src_core_libraries_network_net__util.cpp
@@ -1,40 +1,33 @@
---- src/core/libraries/network/net_util.cpp.orig	2025-09-26 23:09:32 UTC
+--- src/core/libraries/network/net_util.cpp.orig	2025-09-22 18:34:11 UTC
 +++ src/core/libraries/network/net_util.cpp
-@@ -29,12 +29,17 @@ typedef int net_socket;
- #include <array>
- #include <algorithm>
- #include <cstring>
+@@ -8,7 +8,7 @@ typedef int socklen_t;
+ #include <winsock2.h>
+ typedef SOCKET net_socket;
+ typedef int socklen_t;
+-#else
++#elif defined(__FreeBSD__)
+ #include <cerrno>
+ #include <arpa/inet.h>
+ #include <ifaddrs.h>
+@@ -19,13 +19,16 @@ typedef int socklen_t;
+ #include <sys/ioctl.h>
+ #include <sys/socket.h>
+ #include <unistd.h>
 +#include <net/if.h>
 +#include <net/route.h>
-+#include <netinet/in.h>
-+#include <arpa/inet.h>
 +#include <unistd.h>
+ typedef int net_socket;
  #endif
  #if defined(__APPLE__)
  #include <net/if_dl.h>
  #include <net/route.h>
  #endif
 -#if __linux__
-+#if defined(__linux__) || defined(__FreeBSD__)
++#if __linux__ || __FreeBSD__
  #include <fstream>
  #include <iostream>
  #include <sstream>
-@@ -147,11 +152,12 @@ bool NetUtilInternal::RetrieveEthernetAddr() {
-     return false;
- }
- } // namespace NetUtil
--const std::string& NetUtilInternal::GetDefaultGateway() const {
-+
-+const std::string& NetUtil::NetUtilInternal::GetDefaultGateway() const {
-     return default_gateway;
- }
- 
--bool NetUtilInternal::RetrieveDefaultGateway() {
-+bool NetUtil::NetUtilInternal::RetrieveDefaultGateway() {
-     std::scoped_lock lock{m_mutex};
- 
- #ifdef _WIN32
-@@ -253,7 +259,9 @@ bool NetUtilInternal::RetrieveDefaultGateway() {
+@@ -222,7 +225,9 @@ bool NetUtilInternal::RetrieveDefaultGateway() {
      this->default_gateway = str;
      return true;
  
@@ -45,7 +38,7 @@
      std::ifstream route{"/proc/net/route"};
      std::string line;
  
-@@ -283,11 +291,11 @@ bool NetUtilInternal::RetrieveDefaultGateway() {
+@@ -252,11 +257,11 @@ bool NetUtilInternal::RetrieveDefaultGateway() {
      return false;
  }
  
@@ -59,7 +52,7 @@
      std::scoped_lock lock{m_mutex};
      char netmaskStr[INET_ADDRSTRLEN];
      auto success = false;
-@@ -353,11 +361,11 @@ bool NetUtilInternal::RetrieveNetmask() {
+@@ -322,11 +327,11 @@ bool NetUtilInternal::RetrieveNetmask() {
      return success;
  }
  
@@ -73,7 +66,7 @@
      std::scoped_lock lock{m_mutex};
  
      auto sockfd = socket(AF_INET, SOCK_STREAM, 0);
-@@ -398,4 +406,3 @@ bool NetUtilInternal::RetrieveIp() {
+@@ -367,4 +372,3 @@ bool NetUtilInternal::RetrieveIp() {
      return true;
  }
  
