24989a78cb248e6b0cebe6bd954c1d466bd6e1a6
Martin Filla <freebsd@sysctl.cz>
2025-10-25 15:37:34 +0200

    usd: update to v25.11

--- usd/files/patch-pxr_base_arch_fileSystem.h
+++ usd/files/patch-pxr_base_arch_fileSystem.h
@@ -1,13 +1,12 @@
---- pxr/base/arch/fileSystem.h.orig	2025-10-24 16:21:56 UTC
+--- pxr/base/arch/fileSystem.h.orig	2025-10-24 22:00:56 UTC
 +++ pxr/base/arch/fileSystem.h
-@@ -24,9 +24,8 @@
- #include <sys/types.h>
- #include <sys/stat.h>
- 
--#if defined(ARCH_OS_LINUX) || defined(ARCH_OS_WASM_VM)
-+#if defined(ARCH_OS_LINUX) || defined(ARCH_OS_WASM_VM) || defined(ARCH_OS_FREEBSD)
+@@ -28,6 +28,9 @@
  #include <unistd.h>
--#include <sys/statfs.h>
+ #include <sys/statfs.h>
  #include <glob.h>
++#elif defined(ARCH_OS_FREEBSD)
++#include <unistd.h>
++#include <glob.h>
  #elif defined(ARCH_OS_DARWIN)
  #include <unistd.h>
+ #include <sys/mount.h>
