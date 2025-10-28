d17c1117f29cf317a15b2e3bf83c1529bb5688d1
Martin Filla <freebsd@sysctl.cz>
2025-10-28 07:45:40 +0100

    cortex: update to 10.6.1.0

---
+++ cortex/files/patch-src_IECore_PathVectorParameter.cpp
@@ -0,0 +1,10 @@
+--- src/IECore/PathVectorParameter.cpp.orig	2025-10-28 06:45:08 UTC
++++ src/IECore/PathVectorParameter.cpp
+@@ -38,7 +38,6 @@
+ 
+ #include "boost/algorithm/string/classification.hpp"
+ #include "boost/algorithm/string/split.hpp"
+-#include "boost/filesystem/convenience.hpp"
+ #include "boost/filesystem/operations.hpp"
+ 
+ #include <algorithm>
