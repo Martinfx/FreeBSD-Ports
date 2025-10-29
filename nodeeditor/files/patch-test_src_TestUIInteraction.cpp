b3c5bbb13c878dc8ec24f75f198bdb892ccbafab
Martin Filla <freebsd@sysctl.cz>
2025-10-29 08:08:47 +0100

    nodeeditor: update to 3.0.12

--- nodeeditor/files/patch-test_src_TestUIInteraction.cpp
+++ nodeeditor/files/patch-test_src_TestUIInteraction.cpp
@@ -1,14 +1,5 @@
---- test/src/TestUIInteraction.cpp.orig	2025-10-09 16:54:05 UTC
+--- test/src/TestUIInteraction.cpp.orig	2025-10-29 07:53:22 UTC
 +++ test/src/TestUIInteraction.cpp
-@@ -8,7 +8,7 @@
- #include <QtNodes/internal/ConnectionGraphicsObject.hpp>
- #include <QtNodes/Definitions>
- 
--#include <catch2/catch.hpp>
-+#include <catch2/catch_all.hpp>
- #include <QTest>
- #include <QSignalSpy>
- #include <QGraphicsSceneMouseEvent>
 @@ -20,6 +20,7 @@
  #include <QGraphicsScene>
  #include <QTimer>
