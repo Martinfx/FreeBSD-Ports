e46eb20146f2077b514f0b37a3f912a21bcff847
Martin Filla <freebsd@sysctl.cz>
2025-11-17 09:53:54 +0100

    cortex: new port

---
+++ cortex/files/patch-src_IECore_FileNameParameter.cpp
@@ -0,0 +1,10 @@
+--- src/IECore/FileNameParameter.cpp.orig	2025-11-17 08:42:02 UTC
++++ src/IECore/FileNameParameter.cpp
+@@ -39,7 +39,6 @@
+ 
+ #include "boost/algorithm/string/classification.hpp"
+ #include "boost/algorithm/string/split.hpp"
+-#include "boost/filesystem/convenience.hpp"
+ #include "boost/filesystem/operations.hpp"
+ #include "boost/format.hpp"
+ 
