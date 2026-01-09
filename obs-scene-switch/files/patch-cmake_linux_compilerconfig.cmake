626ab239946864a1988751ce29623fc960b9267b
Martin Filla <freebsd@sysctl.cz>
2026-01-09 03:58:08 +0100

    obs-scene-switch: new port
    Signed-off-by: Martin Filla <freebsd@sysctl.cz>

---
+++ obs-scene-switch/files/patch-cmake_linux_compilerconfig.cmake
@@ -0,0 +1,10 @@
+--- cmake/linux/compilerconfig.cmake.orig	2026-01-09 02:16:28 UTC
++++ cmake/linux/compilerconfig.cmake
+@@ -17,7 +17,6 @@ set(_obs_gcc_c_options
+     -Wdeprecated-declarations
+     -Wempty-body
+     -Wenum-conversion
+-    -Werror=return-type
+     -Wextra
+     -Wformat
+     -Wformat-security
