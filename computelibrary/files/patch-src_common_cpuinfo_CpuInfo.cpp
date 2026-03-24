ea7bcd75ad8c5a2c0aadbf80bdcbd2219fa01223
Martin Filla <freebsd@sysctl.cz>
2026-03-24 18:24:03 +0100

    compute-library: fix ncpu and HW_CAPS

--- computelibrary/files/patch-src_common_cpuinfo_CpuInfo.cpp
+++ computelibrary/files/patch-src_common_cpuinfo_CpuInfo.cpp
@@ -9,7 +9,7 @@
  
  #if !defined(BARE_METAL)
  #include <algorithm>
-@@ -52,15 +54,15 @@
+@@ -52,15 +54,16 @@
  #endif /* !defined(BARE_METAL) */
  
  #if !defined(_WIN64)
@@ -22,13 +22,14 @@
 +#elif (defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__APPLE__)) && defined(__aarch64__)
  #include <sys/sysctl.h>
  #include <sys/types.h>
++#include <sys/auxv.h>
  #endif /* defined(__APPLE__) && defined(__aarch64__)) */
 -#endif /* !defined(BARE_METAL) && !defined(__APPLE__) && !defined(__OpenBSD__) && !defined(__QNX__) && (defined(__arm__) || defined(__aarch64__)) */
 +#endif /* !defined(BARE_METAL) && !defined(__APPLE__) && !defined(__OpenBSD__) !defined(__FreeBSD__) && !defined(__QNX__) && (defined(__arm__) || defined(__aarch64__)) */
  
  #define ARM_COMPUTE_CPU_FEATURE_HWCAP_CPUID    (1 << 11)
  #define ARM_COMPUTE_GET_FEATURE_REG(var, freg) __asm __volatile("MRS %0, " #freg : "=r"(var))
-@@ -70,7 +72,7 @@ namespace
+@@ -70,7 +73,7 @@ namespace
  {
  namespace
  {
@@ -37,7 +38,7 @@
      (defined(__arm__) || defined(__aarch64__))
  /** Extract MIDR using CPUID information that are exposed to user-space
   *
-@@ -352,7 +354,7 @@ CpuInfo CpuInfo::build()
+@@ -352,7 +355,7 @@ CpuInfo CpuInfo::build()
  
  CpuInfo CpuInfo::build()
  {
@@ -46,7 +47,7 @@
      (defined(__arm__) || defined(__aarch64__))
      const uint32_t hwcaps   = getauxval(AT_HWCAP);
      const uint32_t hwcaps2  = getauxval(AT_HWCAP2);
-@@ -383,7 +385,7 @@ CpuInfo CpuInfo::build()
+@@ -383,7 +386,7 @@ CpuInfo CpuInfo::build()
  
      CpuInfo info(isa, cpus_model);
      return info;
@@ -55,7 +56,7 @@
      int    mib[2] = {0, 0};
      int    ncpu   = {1};
      size_t len    = sizeof(ncpu);
-@@ -419,11 +421,31 @@ CpuInfo CpuInfo::build()
+@@ -419,11 +422,36 @@ CpuInfo CpuInfo::build()
      std::vector<CpuModel> cpus_model(1, midr_to_model(midr));
      CpuInfo               info(isa, cpus_model);
      return info;
@@ -65,21 +66,26 @@
 +
 +#elif defined(__aarch64__) && (defined(__OpenBSD__) || defined(__FreeBSD__))
 +    /* #elif(BARE_METAL) && defined(__aarch64__) */
-+    int ncpus = get_hw_capability("hw.ncpu");
++    int ncpus = sysconf(_SC_NPROCESSORS_ONLN);
 +
      CpuIsaInfo            isainfo;
      std::vector<CpuModel> cpus_model(ncpus);
-+    isainfo.neon       = get_hw_capability("hw.optional.neon");
-+    isainfo.fp16       = get_hw_capability("hw.optional.neon_fp16");
-+    isainfo.dot        = get_hw_capability("hw.optional.arm.FEAT_DotProd");
-+    isainfo.bf16       = get_hw_capability("hw.optional.arm.FEAT_BF16");
-+    isainfo.i8mm       = get_hw_capability("hw.optional.arm.FEAT_I8MM");
-+    isainfo.sme        = get_hw_capability("hw.optional.arm.FEAT_SME");
-+    isainfo.sme_f32f32 = get_hw_capability("hw.optional.arm.SME_F32F32");
-+    isainfo.sme_b16f32 = get_hw_capability("hw.optional.arm.SME_B16F32");
-+    isainfo.sme_f16f32 = get_hw_capability("hw.optional.arm.SME_F16F32");
-+    isainfo.sme_i8i32  = get_hw_capability("hw.optional.arm.SME_I8I32");
-+    isainfo.sme2       = get_hw_capability("hw.optional.arm.FEAT_SME2");
++
++    unsigned long hwcap = 0, hwcap2 = 0;
++    elf_aux_info(AT_HWCAP, &hwcap, sizeof(hwcap));
++    elf_aux_info(AT_HWCAP2, &hwcap2, sizeof(hwcap2));
++
++    isainfo.neon       = (hwcap & HWCAP_ASIMD) != 0;
++    isainfo.fp16       = (hwcap & HWCAP_FPHP) != 0;
++    isainfo.dot        = (hwcap & HWCAP_ASIMDDP) != 0;
++    isainfo.bf16       = (hwcap2 & HWCAP2_BF16) != 0;
++    isainfo.i8mm       = (hwcap2 & HWCAP2_I8MM) != 0;
++    isainfo.sme        = (hwcap2 & HWCAP2_SME) != 0;
++    isainfo.sme2       = (hwcap2 & HWCAP2_SME2) != 0;
++    isainfo.sme_f32f32 = (hwcap2 & HWCAP2_SME_F32F32) != 0;
++    isainfo.sme_i8i32  = (hwcap2 & HWCAP2_SME_I8I32) != 0;
++    isainfo.sme_f16f32 = (hwcap2 & HWCAP2_SME_F16F32) != 0;
++    isainfo.sme_b16f32 = (hwcap2 & HWCAP2_SME_B16F32) != 0;
 +    CpuInfo info(isainfo, cpus_model);
 +    return info;
 +#elif defined(__aarch64__) && defined(__APPLE__)
@@ -90,7 +96,7 @@
      isainfo.neon = get_hw_capability("hw.optional.neon");
      isainfo.fp16 = get_hw_capability("hw.optional.neon_fp16");
      isainfo.dot  = get_hw_capability("hw.optional.arm.FEAT_DotProd");
-@@ -466,7 +488,7 @@ CpuModel CpuInfo::cpu_model() const
+@@ -466,7 +494,7 @@ CpuModel CpuInfo::cpu_model() const
  
  CpuModel CpuInfo::cpu_model() const
  {
