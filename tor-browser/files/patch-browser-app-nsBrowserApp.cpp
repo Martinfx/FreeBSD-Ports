ea7980f766faff4c5fa1fadfd6a5cc8e2cabf9a5
Martin Filla <freebsd@sysctl.cz>
2026-04-17 09:21:12 +0200

    tor-browser: update to 16.0a5

--- tor-browser/files/patch-browser-app-nsBrowserApp.cpp
+++ tor-browser/files/patch-browser-app-nsBrowserApp.cpp
@@ -1,13 +1,13 @@
-diff --git browser/app/nsBrowserApp.cpp browser/app/nsBrowserApp.cpp
-index 87c2e16..a41f1d9 100644
---- browser/app/nsBrowserApp.cpp
-+++ browser/app/nsBrowserApp.cpp
-@@ -337,6 +337,9 @@ int main(int argc, char* argv[], char* envp[]) {
-   }
- #endif
+--- browser/app/nsBrowserApp.cpp.orig	2024-10-21 21:51:41.000000000 +0200
++++ browser/app/nsBrowserApp.cpp	2024-11-07 21:47:05.301097000 +0100
+@@ -391,6 +391,11 @@
+   // Register an external module to report on otherwise uncatchable exceptions.
+   CrashReporter::RegisterRuntimeExceptionModule();
  
 +  setenv("MOZ_GMP_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
 +  setenv("MOZ_PLUGIN_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
++  setenv("FONTCONFIG_PATH", "%%LOCALBASE%%/lib/tor-browser/fontconfig/", 1);
++  setenv("FONTCONFIG_FILE", "fonts.conf", 1);
 +
  #ifdef HAS_DLL_BLOCKLIST
    DllBlocklist_Initialize(gBlocklistInitFlags);
--- tor-browser/files/patch-toolkit_components_tor-launcher_TorProcess.sys.mjs
+++ tor-browser/files/patch-toolkit_components_tor-launcher_TorProcess.sys.mjs
@@ -1,10 +1,6 @@
-Let geoip/geoip6 file paths be set by prefs like everything else and let
-the new getTorFile() deal with it.
-
-Index: toolkit/components/tor-launcher/TorProcess.sys.mjs
---- toolkit/components/tor-launcher/TorProcess.sys.mjs.orig	2024-04-22 21:47:56 UTC
+--- toolkit/components/tor-launcher/TorProcess.sys.mjs.orig	2026-03-31 18:04:56 UTC
 +++ toolkit/components/tor-launcher/TorProcess.sys.mjs
-@@ -216,6 +216,7 @@ export class TorProcess {
+@@ -253,6 +253,7 @@ export class TorProcess {
      }
  
      this.#args = [];
@@ -12,23 +8,21 @@ Index: toolkit/components/tor-launcher/TorProcess.sys.mjs
      this.#args.push("-f", torrcFile.path);
      this.#args.push("DataDirectory", this.#dataDir.path);
      this.#args.push("ClientOnionAuthDir", onionAuthDir.path);
-@@ -230,11 +231,15 @@ export class TorProcess {
-       // The geoip and geoip6 files are in the same directory as torrc-defaults.
-       // TODO: Change TorFile to return the generic path to these files to make
-       // them independent from the torrc-defaults.
--      const geoipFile = torrcDefaultsFile.clone();
--      geoipFile.leafName = "geoip";
-+      // const geoipFile = torrcDefaultsFile.clone();
-+      // geoipFile.leafName = "geoip";
-+      // this.#args.push("GeoIPFile", geoipFile.path);
-+      // const geoip6File = torrcDefaultsFile.clone();
-+      // geoip6File.leafName = "geoip6";
-+      // this.#args.push("GeoIPv6File", geoip6File.path);
+@@ -275,7 +276,7 @@ export class TorProcess {
+       false
+     );
+     if (torAppDataDir) {
+-      const geoipFile = torAppDataDir.clone();
 +      const geoipFile = lazy.TorLauncherUtil.getTorFile("geoip", false);
+       geoipFile.append("geoip");
+       if (geoipFile.exists()) {
+         this.#args.push("GeoIPFile", geoipFile.path);
+@@ -284,7 +285,7 @@ export class TorProcess {
+           "GeoIP file not found, the circuit display will not show locations."
+         );
+       }
+-      const geoip6File = torAppDataDir.clone();
 +      const geoip6File = lazy.TorLauncherUtil.getTorFile("geoip6", false);
-       this.#args.push("GeoIPFile", geoipFile.path);
--      const geoip6File = torrcDefaultsFile.clone();
--      geoip6File.leafName = "geoip6";
-       this.#args.push("GeoIPv6File", geoip6File.path);
-     } else {
-       logger.warn(
+       geoip6File.append("geoip6");
+       if (geoip6File.exists()) {
+         this.#args.push("GeoIPv6File", geoip6File.path);
